d7cc34d74679a3690600f4e29a57a83a3fe79e87
Viktor Ericson <vcen21@student.aau.dk>
2022-02-20 17:59:29 +0100

    Virker

--- main.py
+++ main.py
@@ -1,13 +1,24 @@
 import requests
-from flask import Flask, render_template
+from flask import Flask, render_template,request
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return render_template('base.html')
 
+
+@app.route('/', methods=['POST'])
+def home():
+    url =  "http://api.weatherapi.com/v1/forecast.json?key=83231fabfb3e4682933162709222002&q={}&days=1&aqi=no&alerts=no"
+    city = request.form.get("city")
+
+    r= requests.get(url.format(city)).json()
+
+    weather1={
+        "city":city,
+        "temperature": r["current"]["temp_c"]
+    }
+    weather=[weather1]
+    return render_template('base.html',weather=weather)
 
 if __name__ == "__main__":
     app.run()
